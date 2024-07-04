a2cacee917a621c1093eb8e0a1b4e33a27a5065c
rbh41594 <rbh415942gmail.com>
2024-07-04 20:06:30 +0530

    completed form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
+import React, { useState } from "react";
+import "./App.css";
 
-import './App.css';
+const Form = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
 
-function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFullName(`${firstName} ${lastName}`);
+  };
+
+  const handleInvalid = (event) => {
+    event.target.setCustomValidity("Please fill out this field.");
+  };
+
+  const handleInput = (event) => {
+    event.target.setCustomValidity("");
+  };
+
   return (
     <div className="App">
-      
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="firstname">First Name:</label>
+          <input
+            type="text"
+            id="firstname"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            onInvalid={handleInvalid}
+            onInput={handleInput}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="firstname">Last Name:</label>
+          <input
+            type="text"
+            id="firstname"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            onInvalid={handleInvalid}
+            onInput={handleInput}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {fullName && (
+        <p>Full Name:{fullName}</p>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default Form;
